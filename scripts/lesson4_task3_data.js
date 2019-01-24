f15be94e58590de827f1e5afb235913cefed3abb
katyaga <aeva_ev@mail.ru>
2019-01-24 14:43:31 +0700

    Михненкова Екатерина ДЗ №4

---
+++ scripts/lesson4_task3_data.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        question: 'Какая страна дала название популярному мультфильму? ',
+        possible_answers: [
+            '1 - Алжир',
+            '2 - Уганда',
+            '3 - Мадагаскар',
+            '4 - Габон',
+        ],
+        answer_id: 2, // index of possible_answers
+        question_amount: 10000,
+    },
+    {
+        question: 'Какое государство не африканское? ',
+        possible_answers: [
+            '1 - Эфиопия',
+            '2 - Кения',
+            '3 - Ботсвана',
+            '4 - Никарагуа',
+        ],
+        answer_id: 3, // index of possible_answers
+        question_amount: 30000,
+    },
+    {
+        question: 'Выберите страну, у которой название совпадает с названием столицы: ',
+        possible_answers: [
+            '1 - Египет',
+            '2 - Марокко',
+            '3 - Нигерия',
+            '4 - Тунис',
+        ],
+        answer_id: 3, // index of possible_answers
+        question_amount: 50000,
+    },
+    {
+        question: 'Главные месторождения алмазов разрабатываются в: ',
+        possible_answers: [
+            '1 - ЮАР',
+            '2 - Заир',
+            '3 - Конго',
+            '4 - Чад',
+        ],
+        answer_id: 0, // index of possible_answers
+        question_amount: 100000,
+    },
+    {
+        question: 'Территория какого государства в 20 веке являлась Британской колонией? ',
+        possible_answers: [
+            '1 - Сомали',
+            '2 - Мадагаскар',
+            '3 - Египет',
+            '4 - Тунис',
+        ],
+        answer_id: 2, // index of possible_answers
+        question_amount: 200000,
+    },
+    {
+        question: 'Алжир и Ливия – государства, которые добывают : ',
+        possible_answers: [
+            '1 - железные руды',
+            '2 - нефть и газ',
+            '3 - титановые руды',
+            '4 - фосфориты',
+        ],
+        answer_id: 1,  // index of possible_answers
+        question_amount: 300000,
+    },
+    {
+        question: 'Какое государство по форме правления является конституционной монархией: ',
+        possible_answers: [
+            '1 - Марокко',
+            '2 - Сенегал',
+            '3 - Намибия',
+            '4 - Гана',
+        ],
+        answer_id: 0, // index of possible_answers
+        question_amount: 500000,
+    },
+    {
+        question: 'Родина СПИДа, эпицентр его распространения: ',
+        possible_answers: [
+            '1 - Северная Африка',
+            '2 - Тропическая Африка',
+            '3 - Экваториальная Африка',
+            '4 - Южная Африка',
+        ],
+        answer_id: 1, // index of possible_answers
+        question_amount: 700000,
+    },
+    {
+        question: 'В Северной Африке исповедуют: ',
+        possible_answers: [
+            '1 - католицизм',
+            '2 - буддизм',
+            '3 - синтоизм',
+            '4 - ислам',
+        ],
+        answer_id: 3, // index of possible_answers
+        question_amount: 850000,
+    },
+    {
+        question: 'В 19 веке американец Генри Стенли открыл реку: ',
+        possible_answers: [
+            '1 - Заир',
+            '2 - Конго',
+            '3 - Кагера',
+            '4 - Нил',
+        ],
+        answer_id: 2, // index of possible_answers
+        question_amount: 1000000,
+    },
+];
